858878de830af90d07b11ad3d9d06d71cfa2f8a7
Longistamina <dophamthelong03@gmail.com>
2025-11-06 19:12:27 +0900

    prepared for 23_Numeric, reordered files

---
+++ 05_Pandas_DataR_dataframe/03_DataR_dplyr_style/23_Numeric_conversion.py
@@ -0,0 +1,19 @@
+'''
+1. dr.as_intege(): Convert to Integer
+
+2. dr.as_double(): Convert to Double
+
+3. dr.as_numeric(): Convert to Numeric
+'''
+
+import datar.all as dr
+from datar import f
+import pandas as pd
+
+from pipda import register_verb
+dr.filter = register_verb(func = dr.filter_)
+dr.slice = register_verb(func = dr.slice_)
+
+# Suppress all warnings
+import warnings
+warnings.filterwarnings("ignore")
