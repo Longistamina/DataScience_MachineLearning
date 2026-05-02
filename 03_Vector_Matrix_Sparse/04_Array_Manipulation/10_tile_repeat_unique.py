ba2d8606faa9af13f8211cd262b67d0a4e749ede
Longistamina <dophamthelong03@gmail.com>
2026-05-02 09:30:44 +0900

    updated at 09:30:44 2026-05-02

--- 03_Vector_Matrix_Sparse/04_Array_Manipulation/10_tile_repeat_unique.py
+++ 03_Vector_Matrix_Sparse/04_Array_Manipulation/10_tile_repeat_unique.py
@@ -1,6 +1,7 @@
 '''
 1. np.tile() - Repeat an array along specified axes
 2. np.repeat() - Repeat elements of an array
+3. np.unique() - Return unique elements of an arrays
 '''
 
 import numpy as np
@@ -159,3 +160,80 @@ result = np.repeat(matrix, 2, axis=1)
 print(result)
 # [[4 4 5 5 6 6]
 #  [7 7 8 8 9 9]]
+
+
+#---------------------------------------------------------------------------------------------------------------#
+#--------------------------------------------- 3. np.unique() --------------------------------------------------#
+#---------------------------------------------------------------------------------------------------------------#
+'''
+np.unique(ar, return_index=False, return_inverse=False, return_counts=False, axis=None)
+
+ar: input array
+return_index: if True, return the indices of the unique values in the flattened original array
+return_inverse: if True, return the indices of the unique values that can be used to reconstruct the original array
+return_counts: if True, return the number of times each unique value appears in the original array
+sorted: if True, sort the unique values in ascending order
+'''
+
+vector_repeat = np.repeat(vector, 2)
+print(vector_repeat)
+# [1 1 2 2 3 3]
+
+matrix_tile = np.tile(matrix, (2, 3))
+print(matrix_tile)
+# [[4 5 6 4 5 6 4 5 6]
+#  [7 8 9 7 8 9 7 8 9]
+#  [4 5 6 4 5 6 4 5 6]
+#  [7 8 9 7 8 9 7 8 9]]
+
+#################
+## default use ##
+#################
+'''Only return the sorted unique values'''
+
+result = np.unique(vector_repeat)
+print(result)
+# [1 2 3]
+
+result = np.unique(matrix_tile)
+print(result)
+# [4 5 6 7 8 9]
+
+#########################
+## unique_indices=True ##
+#########################
+'''Return the indices of the unique values in the flattened original array'''
+
+result = np.unique(matrix_tile, return_index=True)
+print(result)
+# (array([4, 5, 6, 7, 8, 9]),   -> unique values
+#  array([0, 1, 2, 9, 10, 11])) -> indices in the flattened original array
+
+#########################
+## unique_inverse=True ##
+#########################
+'''Return the indices of the unique values that can be used to reconstruct the original array'''
+
+result = np.unique(matrix_tile, return_inverse=True)
+print(result)
+# (array([4, 5, 6, 7, 8, 9]),         -> unique_values
+# array([[0, 1, 2, 0, 1, 2, 0, 1, 2], -> indices that can be used to reconstruct the original array
+#        [3, 4, 5, 3, 4, 5, 3, 4, 5],
+#        [0, 1, 2, 0, 1, 2, 0, 1, 2],
+#        [3, 4, 5, 3, 4, 5, 3, 4, 5]]))
+
+unique_values, inverse_indices = result
+print(unique_values[inverse_indices])
+# [[4 5 6 4 5 6 4 5 6]
+#  [7 8 9 7 8 9 7 8 9]
+#  [4 5 6 4 5 6 4 5 6]
+#  [7 8 9 7 8 9 7 8 9]]
+
+########################
+## return_counts=True ##
+########################
+'''Return the number of times each unique value appears in the original array'''
+
+result = np.unique(matrix_tile, return_counts=True)
+print(result)
+# (array([4, 5, 6, 7, 8, 9]), array([6, 6, 6, 6, 6, 6]))
