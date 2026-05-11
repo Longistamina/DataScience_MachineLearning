2f930d60a5e0008769ab648e8081e41f7ed53fe8
Longistamina <dophamthelong03@gmail.com>
2026-05-11 20:04:59 +0900

    updated at 20:04:59 2026-05-11

--- 05_Numba_PythonCompiler/01_jit_compilation_signatures.py
+++ 05_Numba_PythonCompiler/01_jit_compilation_signatures.py
@@ -14,6 +14,7 @@ Various invocation modes trigger differing compilation options and behaviours.
 '''
 
 import numba as nb
+import numpy as np
 import math
 
 
@@ -62,9 +63,9 @@ For example:
 NOTE: if the input types are not the same as the given signatures, numba will force them into the signatures
 '''
 
-##################################
-## Example output(input, input) ##
-##################################
+####################################
+## Example (output(input, input)) ##
+####################################
 
 @nb.jit(nb.float32(nb.int64, nb.int64)) # Eager compilation right here, before the first execution
 def sum_square(x, y):
@@ -91,9 +92,56 @@ print(result)
 # (4.5-9j)
 '''Here, numba automatically infer the signature of the output as complex'''
 
+############################################
+## Example (output(scalar, array, array)) ##
+############################################
+
+@nb.jit(nb.float64[:, :](nb.float64, nb.int32[:, :], nb.float32[:, :])) # ``nb.int32[:, :]`` means a 2D array with int32 type
+def simple_array_operation(scalar, arr1, arr2):
+    output = scalar * arr1 + arr2 # Result in a nb.float64
+    return output
+
+'''
+Here, must define the output as ``nb.float64[:, :]``, because one input (the scalar) is nb.float64,
+so all the computings will result in ``nb.float64``.
+(the ``output`` becomes ``nb.float64[:, :]``)
+
+If we use ``nb.float32[:, :](nb.float64, nb.int32[:, :], nb.float32[:, :])``,
+the ``nb.float32[:, :]`` inside the ``@nb.jit()`` will be mismatched with the type of the ``output``
+-> error
+'''
+
+#-------#
+
+np.random.seed(42)
+arr1 = np.random.randint(30, size=(4, 3)).astype(np.int32)
+print(arr1)
+# [[ 6 19 28]
+#  [14 10  7]
+#  [28 20  6]
+#  [25 18 22]]
+
+np.random.seed(24)
+arr2 = np.random.randn(4, 3).astype(np.float32)
+print(arr2)
+# [[ 1.3292122  -0.7700335  -0.31628036]
+#  [-0.9908104  -1.0708163  -1.4387133 ]
+#  [ 0.5644168   0.2957219  -1.6264043 ]
+#  [ 0.2195652   0.6788048   1.8892727 ]]
+
+result = simple_array_operation(
+    scalar=math.pi,
+    arr1=arr1,
+    arr2=arr2
+)
+print(result)
+# [[20.17876811 58.92022694 87.64831394]
+#  [42.99148676 30.34511026 20.55243526]
+#  [88.52901113 63.12757496 17.22315164]
+#  [78.75938154 57.22747258 71.00431107]]
 
 #------------------------------------------------------------------------------------------------------#
-#-------------------------------------- 2. Numba signatures -------------------------------------------#
+#-------------------------------------- 3. Numba signatures -------------------------------------------#
 #------------------------------------------------------------------------------------------------------#
 '''
 Explicit ``@jit`` signatures can use a number of types.  Here are some common ones:
@@ -113,4 +161,42 @@ Explicit ``@jit`` signatures can use a number of types.  Here are some common on
 
 * array types can be specified by indexing any numeric type, e.g. ``float32[:]`` for a one-dimensional single-precision array
                                                                 or ``int8[:,:]`` for a two-dimensional array of 8-bit integers.
+
+##############################
+
+int64 wins int32 wints int16 ...
+float64 wins float32 wins float16 ...
 '''
+
+
+#-------------------------------------------------------------------------------------------------------#
+#----------------------------- 4. Calling and inlining other functions ---------------------------------#
+#-------------------------------------------------------------------------------------------------------#
+'''
+Numba-compiled functions can call other compiled functions,
+and can also be called by other compiled functions.
+
+Numba-compiled functions can also be inlined by other native functions
+(depending on optimizer heuristics.)
+'''
+
+@nb.jit
+def square(x):
+    return x**2
+
+@nb.jit
+def sum_sqr(nums):
+    sum = 0
+    for x in nums:
+        sum += square(x) # call a numba-compiled function inside another compiled function
+    return sum
+
+arr_inputs = np.array([2, 3, 5, 9.2])
+sum_of_square = sum_sqr(arr_inputs) # Should prioritize np.array as inputs, because numba likes this
+print(sum_of_square)
+# 122.63999999999999
+
+arr_inputs = np.array([5, 3.2, 8, 7.3])
+sqrt_sum_of_square = math.sqrt(sum_sqr(arr_inputs)) # inline a numba-compiled function inside a native function
+print(sum_of_square)
+# 12.350303639992015
